d799c55a45b88b651936b9f4b40aca4dd399f013
WilderWine <vernikon72@gmail.com>
2022-08-31 02:36:51 +0300

    добавлен бассейн с шариками, но надо доработать сцену

--- Assets/Scripts/WindowFallGame/studentMove.cs
+++ Assets/Scripts/WindowFallGame/studentMove.cs
@@ -7,7 +7,9 @@ public class studentMove : MonoBehaviour
 {
 
     public float speed;
+    public float waiting;
     private float moveInput;
+    private SpriteRenderer studentRenderer;
     public GameObject escapePanel;
     public GameObject failPanel;
     public GameObject birds;
@@ -17,6 +19,7 @@ public class studentMove : MonoBehaviour
     void Start()
     {
         fallingBody = GetComponent<Rigidbody2D>();
+        studentRenderer = GetComponent<SpriteRenderer>();
     }
 
 
@@ -24,17 +27,21 @@ public class studentMove : MonoBehaviour
     {
         if(other.tag == "ground")
         {
-
+            StartCoroutine(IntroWait());
+            studentRenderer.color = new Color(1f, 1f, 1f, 0f);
             Destroy(birds);
 
-            escapePanel.SetActive(true);
-            Destroy(this.gameObject);
+            this.GetComponent<Transform>().localScale = new Vector2(0, 0);
+            
 
             PlayerPrefs.SetInt("money", 0);
             PlayerPrefs.SetFloat("sleep", 1f);
             PlayerPrefs.SetFloat("food", 1f);
             PlayerPrefs.SetFloat("toilet", 1f);
             PlayerPrefs.SetFloat("study", 1f);
+
+
+            
             //     р с р    м ю д н    я д е к ю р э    окщепопетя    я     н а м с к е м х е л    б я е ц н 
         }
 
@@ -57,5 +64,16 @@ public class studentMove : MonoBehaviour
         if (fallingBody.position.x >= 2.75f) fallingBody.position = new Vector2(2.75f, fallingBody.position.y);
         if (fallingBody.position.x <= -2.75f) fallingBody.position = new Vector2(-2.75f, fallingBody.position.y);
         fallingBody.rotation = fallingBody.velocity.x * 5f;
+    }
+
+
+    IEnumerator IntroWait()
+    {
+        yield return new WaitForSeconds(waiting);
+        escapePanel.SetActive(true);
+        GameObject _balls = GameObject.Find("poolWithBalls");
+        
+        Destroy(_balls.gameObject);
+        Destroy(this.gameObject);
     }
 }
