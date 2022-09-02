ab7cdd5861fc3f3e17e4c5a46d879b5a3d87a1d3
chamomil <alyonka.m.by@gmail.com>
2022-09-02 20:52:46 +0300

    мини-игра доработана

--- Assets/Scripts/bossGame/PlayerMove.cs
+++ Assets/Scripts/bossGame/PlayerMove.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
+
 
 public class PlayerMove : MonoBehaviour
 {
@@ -12,16 +14,18 @@ public class PlayerMove : MonoBehaviour
     private float distanceX = 0f;
     private float speed = 5f;
     [SerializeField] int lives = 3;
-    [SerializeField] int timeToSurvive = 60;
+    [SerializeField] int timeToSurvive = 61;
     private bool isDead = false;
     private int money;
 
     private Vector3 screenLeft, screenRight;
+    private float objectWidth;
 
     private void Start()
     {
-        screenLeft = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
-        screenRight = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
+        screenLeft = Camera.main.ViewportToWorldPoint(new Vector2(0f, 0f));
+        screenRight = Camera.main.ViewportToWorldPoint(new Vector2(1f, 1f));
+        objectWidth = transform.GetComponent<SpriteRenderer>().bounds.size.x / 2;
         money = PlayerPrefs.GetInt("money");
         StartCoroutine(WinGame());
     }
@@ -52,11 +56,10 @@ public class PlayerMove : MonoBehaviour
     {
         float positionX = rigidbody.position.x + distanceX * speed * Time.fixedDeltaTime;
 
-        if (positionX < screenRight.x && positionX > screenLeft.x)
+        if (positionX < screenRight.x - objectWidth && positionX > screenLeft.x + objectWidth)
         {
             rigidbody.MovePosition(new Vector2(positionX, rigidbody.position.y));
-        }
-        
+        }        
     }
 
     private void Awake()
@@ -78,7 +81,6 @@ public class PlayerMove : MonoBehaviour
             if (lives == 0)
             {
                 isDead = true;
-                //вызов анимации смерти, если такая будет
 
                 SceneResult();
             }
@@ -90,6 +92,8 @@ public class PlayerMove : MonoBehaviour
         onSceneEnd?.Invoke();
         string sceneName = isDead ? "LabFailed" : "LabPassed";
         money += 40;
+        int delta = money - PlayerPrefs.GetInt("money");
+        PlayerPrefs.SetInt("delta", delta);
         PlayerPrefs.SetInt("money", money);
         StartCoroutine(EndWait(sceneName));
     }
