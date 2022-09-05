43bcd108cc7ab6437ed8cd42dc4981d13003a65a
chamomil <alyonka.m.by@gmail.com>
2022-09-05 20:22:02 +0300

    фикс кнопки exit

--- Assets/Scripts/StudyRoom/StudyScript.cs
+++ Assets/Scripts/StudyRoom/StudyScript.cs
@@ -63,6 +63,10 @@ public class StudyScript : MonoBehaviour
         {
             WindowButton.SetActive(true);
         }
+        else
+        {
+            WindowButton.SetActive(false);
+        }
     }
 
     public void IncreaseStudyProgress(float study)
